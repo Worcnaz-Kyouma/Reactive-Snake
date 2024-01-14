7fbf791833a816a4a12e5f129b68bda23cd5dd18
Worcnaz Prado <108674581+Worcnaz-Kyouma@users.noreply.github.com>
2024-01-14 14:16:25 -0300

    feat: old version wrap

--- src/main.tsx
+++
@@ -1,8 +0,0 @@
-import ReactDOM from 'react-dom/client'
-import Game from './components/Game/Game'
-
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <Game />
-)
